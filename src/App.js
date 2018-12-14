4138b907ff6acbbeb06a5e4f4b3b73163fc00d73
Kit Masaracchia <kit.masaracchia@gmail.com>
2018-12-14 00:13:37 -0700

    decide on vanilla css; add fadeIn, flicker keyframes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import styled, { keyframes } from 'styled-components';
 
-class App extends Component {
+// export const fadeIn = keyframes`
+//   0% { opacity: 0; }
+//   100% { opacity: 1; }
+// `;
+
+// const Wrapper = styled.section`
+//   height: 100vh;
+//   width: 100vw;
+//   background-color: black;
+//   animation: ${fadeIn} 2s 2s forwards;
+// `;
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="page-wrapper">
+        <h1 id="intro-title">Intro To Animation</h1>
       </div>
     );
   }
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const colors = {
+  lightGrey: '#EEEEEE',
+  darkGrey: '#4A4A4A',
+  purple: '#A450E3',
+  orange: '#F5A623',
+}
